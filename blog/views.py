afcc1bfffc7dd429211657c8a99d48b96b995d3a
Tito-sam <totitron20@gmail.com>
2022-01-15 16:48:48 -0500

    Terminado el crud

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,24 @@
 from multiprocessing import context
-from django.shortcuts import render, redirect
-from django.views.generic import View
+from re import template
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import View, UpdateView, DeleteView
 from .forms import PostCreateForm
 from .models import Post
+from django.urls import reverse_lazy
 
+#enlistar post
 class BlogListView(View):
     def get(self, request, *args, **wkargs):
-        context = {}
+        # se obtienen los posts de la base de datos
+        posts = Post.objects.all()
+        #se le pasan los post al contexto para la pagina
+        context = {
+            'posts':posts
+        }
+        # renderisamos todo con render
         return render(request, 'blog_list.html', context)
 
-
+#crear un post
 class BlogCreateView(View):
     def get(self, request, *args,**wkargs):
         form = PostCreateForm()
@@ -36,3 +45,28 @@ class BlogCreateView(View):
                 return redirect('blog:home')
         context = {}
         return render(request, 'blog_create.html', context)
+
+#detallar el post
+class BlogDetailView(View):
+    def get(self, request, pk, *args, **kwargs):
+        post = get_object_or_404(Post, pk = pk)
+        context = {
+            'post':post
+        }
+        return render(request, 'blog_detail.html', context)
+
+#actualizar el post
+class BlogUpdateView(UpdateView):
+    model=Post
+    fields=['title','content']
+    template_name = 'blog_update.html'
+
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+        return reverse_lazy('blog:detail', kwargs={'pk':pk})
+
+#eliminar el post
+class BlogDeleteView(DeleteView):
+    model=Post
+    template_name = 'blog_delete.html'
+    success_url = reverse_lazy('blog:hole')
